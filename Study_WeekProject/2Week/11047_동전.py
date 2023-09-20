3c2a10109ff8ad498305fdf2861f48a5530498bd
Kim Jung Hwa <91521370+LUCY-JungHwaKim@users.noreply.github.com>
2023-09-20 11:41:12 +0900

    Update 11047_동전.py

--- Study_WeekProject/2Week/11047_동전.py
+++ Study_WeekProject/2Week/11047_동전.py
@@ -8,7 +8,7 @@ cnt = 0
 
 while k > 0:
 
-    if lst[idx] <= k: ## k보다 동전 수가 더 크면 계산 X
+    if lst[idx] <= k: ## k가 동전 수보다 더 크면 계산 X
         cnt += (k // lst[idx]) ## k를 동전 가치로 나눈 것의 몫이 사용할 수 있는 동전 개수
 
         cur_n = k % lst[idx] ## 그거에 대한 나머지가 다음 k 값이 됨
