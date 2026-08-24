31cea9c132bd6015848273a002c8d726a0aa39e1
jeonghwa.kim <eichi3737@gmail.com>
2026-08-25 01:50:14 +0900

    Solve programmers 12909

---
+++ 프로그래머스/8m:4w/괄호.py
@@ -0,0 +1,25 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+from collections import *
+
+
+def solution(s):
+    answer = True
+
+    queue = deque()
+    cur_s = ""
+
+    for idx, i in enumerate(list(s)):
+        if (i == ")") & (idx == 0):
+            return False
+        elif i == "(":
+            queue.append(i)
+            cur_s = i
+        else:
+            if (len(queue) > 0) & (cur_s == "("):
+                queue.pop()
+
+    if len(queue) == 0:
+        return True
+    else:
+        return False
